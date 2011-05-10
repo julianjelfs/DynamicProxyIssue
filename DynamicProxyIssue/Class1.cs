96715d6e0985a93cd851c72989bc04f2a3f7db57
Julian Jelfs <julian.jelfs@gmail.com>
2011-05-10 08:44:13 +0100

    first commit of dynamic proxy issue demo

---
+++ DynamicProxyIssue/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.EnterpriseServices;
+using Interfaces;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace DynamicProxyIssue
+{
+   
+
+    [TestFixture]
+    public class Class1
+    {
+        [TestFixtureSetUp]
+        public void BeforeFixture()
+        {
+            var int1 = MockRepository.GenerateStub<Interface1>();
+
+            ServiceDomain.Enter(new ServiceConfig
+            {
+                Transaction = TransactionOption.Required,
+                TrackingAppName = "Test",
+                TransactionDescription = "Test",
+                TransactionTimeout = 30
+            });
+
+//            var name = typeof (Interface2).Assembly.GetName(true);
+//            var a = AppDomain.CurrentDomain.DefineDynamicAssembly(typeof (Interface2).Assembly.GetName(true), 
+//                AssemblyBuilderAccess.RunAndSave);
+            
+            var int2 = MockRepository.GenerateStub<Interface2>();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
---
+++ Interfaces/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface Interface1
+    {
+
+    }
+
+    public interface Interface2
+    {
+
+    }
+}
